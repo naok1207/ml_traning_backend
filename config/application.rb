a736ba68b60e65dc4a177a05891e20b97f8c4656
東直希 <azumanaoki@azumanaokinoMacBook-Pro.local>
2022-05-11 08:10:45 +0900

    graphiql動作設定

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,9 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # graphiql動作設定
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
