1c94567c997fb07d1b7470a61368a98b77f1b36c
東直希 <azumanaoki@azumanaokinoMacBook-Pro.local>
2022-05-18 12:31:55 +0900

    タスクの削除のmutationを作成

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
 
     field :create_task, mutation: Mutations::TaskCreate
     field :update_task, mutation: Mutations::TaskUpdate
+    field :delete_task, mutation: Mutations::TaskDelete
   end
 end
