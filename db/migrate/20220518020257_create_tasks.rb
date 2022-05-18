b52b30672e791e9c5f2429f63c4e10f1cf11ac83
東直希 <azumanaoki@azumanaokinoMacBook-Pro.local>
2022-05-18 11:14:37 +0900

    task.statusのdefault値を1から0に修正

--- db/migrate/20220518020257_create_tasks.rb
+++ db/migrate/20220518020257_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
     create_table :tasks do |t|
       t.string :title, null: false
       t.text :description
-      t.integer :status, null: false, default: 1
+      t.integer :status, null: false, default: 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_020257) do
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
-    t.integer "status", default: 1, null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
