df31533691169d4f58dacc8ebe7aba0e5bbca241
東直希 <azumanaoki@azumanaokinoMacBook-Pro.local>
2022-05-18 12:00:21 +0900

    task一覧・詳細取得用のqueryを作成

---
+++ app/graphql/object_types/task.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module ObjectTypes
+  class Task < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :description, String
+    field :status, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,5 +4,8 @@ module Types
     # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
+
+    field :tasks, resolver: Queries::Tasks
+    field :task,  resolver: Queries::Task
   end
 end
