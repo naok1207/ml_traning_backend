bce5d808104cd1f4ef5dbb686b249a9501d6707b
東直希 <azumanaoki@azumanaokinoMacBook-Pro.local>
2022-05-26 15:34:58 +0900

    devise routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   post '/graphql', to: 'graphql#execute'
 end
