f07ffbab1e4a9eef02b700c195ebf8ca7873739c
東直希 <azumanaoki@azumanaokinoMacBook-Pro.local>
2022-05-27 11:24:24 +0900

    Authエラーハンドリングを修正

--- app/graphql/resolvers/auth_query.rb
+++ app/graphql/resolvers/auth_query.rb
@@ -1,7 +1,12 @@
 module Resolvers
   class AuthQuery < Resolvers::BaseQuery
     def authorized?(*_args)
-      raise GraphQL::ExecutionError, 'login required!!' unless context[:current_user]
+      raise GraphQL::ExecutionError.new(
+        'permission denied',
+        extensions: {
+          code: 'AUTHENTICATION_ERROR'
+        }
+      ) unless context[:current_user]
 
       true
     end
